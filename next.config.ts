6400ae02e64d21d1df68b8ea758bd4b33f543911
Riki Itokazu <itokazuriki@gmail.com>
2026-04-23 11:07:28 -0400

    release: change deployment options to use github pages

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,12 @@ import createMDX from '@next/mdx';
 
 const nextConfig: NextConfig = {
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'mdx'],
+  output: 'export', // Enables static export
+  basePath: '/ipeds-data-viz', // Replace with your GitHub repository name
+  images: {
+    unoptimized: true, // Required because Next.js Image Optimization needs a server
+  },
 };
-
 const withMDX = createMDX({});
 
 export default withMDX(nextConfig);
